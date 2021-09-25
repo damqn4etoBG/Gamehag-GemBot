bb64d03693a7c48d619d5daac3dbbae3b12e4cfe
damqn4etoBG <79470838+damqn4etoBG@users.noreply.github.com>
2021-09-25 20:12:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ url = ("https://gamehag.com/tv-zone")
 duration = ("20")
 
 while True:
-    for i in range(40): # Kolko Taba da otvori
+    for i in range(40): # How many tabs to open
         webbrowser.open(url)
         time.sleep(int(duration))
 
 
-    time.sleep(90) # Kolko vreme sled kogato e prikluchilo
+    time.sleep(90) # How much time after the last tab is opened to kill chrome
     os.system("taskkill /im chrome.exe /f") # Kills Chrome
 
 
